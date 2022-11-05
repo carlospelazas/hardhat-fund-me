5095aab1673e976ff891f19ddfac386526ba1f27
carlospelazas <carlospelazas@gmail.com>
2022-11-05 21:33:31 +0100

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+PRIVATE_KEY = process.env.PRIVATE_KEY;
+COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-reportError.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+};
